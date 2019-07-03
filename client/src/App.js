f97c026b5ae6960c7eefcf4e7b10e590d958569e
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-07-03 19:18:56 -0400

    removed navbar and added title and some styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Books from './pages/Books';
 import NoMatch from './pages/NoMatch';
 import Saved from './pages/Saved';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 
 function App() {
   return (
     <Router>
       <>
-        <Nav />
+        {/* <Nav /> */}
         <Switch>
           <Route exact path="/" component={Books} />
           <Route exact path="/saved" component={Saved} />
--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,16 +3,18 @@ import "./style.css";
 
 function Jumbotron() {
   return (
+    <div>
+    <h1>Google Book Search</h1>
+    <p className="lead">Find your next great read!</p>
     <header className="masthead">
       <div className="container h-100">
         <div className="row h-100 align-items-center">
           <div className="col-12 text-center">
-            <h1 className="font-weight-light">Google Book Search</h1>
-            <p className="lead">Find your next great read!</p>
           </div>
         </div>
       </div>
     </header>
+    </div>
   );
 }
 
